ef62b573b42131701c630af074881564f1d77051
EsmaeeilMoradi <esmaeeilmoradi700@gmail.com>
2024-10-05 11:33:31 +0330

    App class and theme, Model classes, UIComponents  Added.
    If you cant build don't worry, this commit add only necessary resources.

--- Jetsnack-clone/app/src/main/java/com/esm/jetsnack_clone/ui/MainActivity.kt
+++ Jetsnack-clone/app/src/main/java/com/esm/jetsnack_clone/ui/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.esm.jetsnack_clone.ui.JetsnackAppClone
-import com.esm.jetsnack_clone.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
         setContent {
-            JetsnackAppClone()
+            JetsnackApp()
         }
     }
 }
--- Jetsnack-clone/app/src/main/java/com/esm/jetsnack_clone/ui/theme/Shape.kt
+++ Jetsnack-clone/app/src/main/java/com/esm/jetsnack_clone/ui/theme/Shape.kt
@@ -1,12 +1,12 @@
 package com.esm.jetsnack_clone.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Shapes
+import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(percent = 50),
+    medium = RoundedCornerShape(20.dp),
     large = RoundedCornerShape(0.dp)
 )
